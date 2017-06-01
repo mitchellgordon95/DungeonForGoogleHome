288d1b20314f47f49148b355df030c166ba75c6d
Mitchell Gordon <mitchell.gordon95@gmail.com>
2017-06-01 17:12:23 +0000

    Adding health endpoint

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ expressApp.post('/', function(req, res) {
     app.handleRequest(actionMap);
 });
 
+expressApp.get('/health', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Still up');
+    res.end();
+});
+
 var options = {
     ca: fs.readFileSync('/home/ubuntu/https_certs/dungeon_mitchgordon_me.ca-bundle'),
     key: fs.readFileSync('/home/ubuntu/https_certs/dungeon_mitchgordon_me.key'),
