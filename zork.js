e3401a13f8b3dbf56b9f0a35bf33b9a07f2f110a
Mitchell Gordon <mitchell.gordon95@gmail.com>
2017-08-31 15:27:56 +0000

    Multiple misunderstood commands => prompt to try help section

--- zork.js
+++ zork.js
@@ -3,7 +3,8 @@ var path = require('path');
 var files = require('./files.js');
 var reading = require('./reading.js');
 
-// TODO - validate conversation / user ids for injection?
+const WRONG_LAST_TIME_KEY = 'wrong_last_time';
+
 exports.doReturningUserLook = function(app) {
     doCommand(app, true);
 }
@@ -11,8 +12,10 @@ exports.handleInput = function(app) {
     doCommand(app, false);
 }
 
+// TODO - validate commands for injection?
 function doCommand(app, isReturningUserLook) {
     var command = isReturningUserLook ? "look" : app.getRawInput();
+    var dialogue_state = app.getDialogState();
 
     command = command.replace(/(south|north).?(east|west)/i, "$1 $2");
     var save_file = files.getUserSaveFile(app);
@@ -28,20 +31,27 @@ function doCommand(app, isReturningUserLook) {
         }
 
         if (response.includes('I don\'t understand that.')) {
-            response = `${response} I thought you said, "${command}". Returning to game.`;
+            if (dialogue_state[WRONG_LAST_TIME_KEY]) {
+                response = `${response} I thought you said, "${command}". Dungeon is an old game that only understands simple commands.  If you haven't played before, we recommend browsinging the help section by saying help. Returning to game. What do you do next?`;
+                app.askSSML(response);
+            } else {
+                response = `${response} I thought you said, "${command}". Returning to game. What do you do next?`;
+                dialogue_state[WRONG_LAST_TIME_KEY] = true;
+                app.askSSML(response, dialogue_state);
+            }
+        } else {
+            var pages = reading.makePagesForZorkOutput(response);
+            if (pages.length === 1) {
+                response = response + ' <break time="1s"/>What do you do next?';
+                app.askSSML(response);
+            } else {
+                // Note: the page handler should add "What do you do next" by itself
+                dialogue_state[reading.CURRENT_PAGE_KEY] = 0;
+                dialogue_state[reading.PAGES_KEY] = pages;
+                app.askWithListSSML(pages[0], reading.makePagesOptions(app), dialogue_state);
+            }
         }
 
-        var pages = reading.makePagesForZorkOutput(response);
-        if (pages.length === 1) {
-            response = response + ' <break time="1s"/>What do you do next?';
-            app.askSSML(response);
-        } else {
-            // Note: the page handler should add "What do you do next" by itself
-            var dialogue_state = {};
-            dialogue_state[reading.CURRENT_PAGE_KEY] = 0;
-            dialogue_state[reading.PAGES_KEY] = pages;
-            app.askWithListSSML(pages[0], reading.makePagesOptions(app), dialogue_state);
-        }
     });
 
     zork.stderr.on('data', (data) => {
