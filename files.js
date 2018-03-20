66117b73266aeacd98552341a1e17cf7c790fe76
Mitchell Gordon <mitchell.gordon95@gmail.com>
2018-03-20 19:23:36 +0000

    Make sure user ids (which we use for file names) don't have slashes

--- files.js
+++ files.js
@@ -4,13 +4,13 @@ exports.SAVE_DIRECTORY = "saved_games";
 exports.LOGS_DIRECTORY = "logs";
 
 exports.getCheckpointSaveFile = function(app) {
-    return path.resolve(exports.SAVE_DIRECTORY + "/user-" + app.getUser().user_id + "-checkpoint");
+    return path.resolve(exports.SAVE_DIRECTORY + "/user-" + app.getUser().user_id.replace('/', '') + "-checkpoint");
 }
 
 exports.getUserSaveFile = function(app) {
-    return path.resolve(exports.SAVE_DIRECTORY + "/user-" + app.getUser().user_id);
+    return path.resolve(exports.SAVE_DIRECTORY + "/user-" + app.getUser().user_id.replace('/', ''));
 }
 
 exports.getUserInputLogFile = function(app) {
-    return path.resolve(exports.LOGS_DIRECTORY + "/user-" + app.getUser().user_id);
+    return path.resolve(exports.LOGS_DIRECTORY + "/user-" + app.getUser().user_id.replace('/', ''));
 }
